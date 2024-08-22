b087cd9e2cb63638c97c8fe481d4a4fc2154f0bb
henriquexpaulo <phvieira3@gmail.com>
2024-08-22 13:34:41 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+
+const App = () => {
+    const [form, setForm] = React.useState({
+      nome: '',
+      email: '',
+    })
+
+    function handleSubmit(event){
+      event.preventDefault();
+      console.log(event);
+    }
+
+    function handleChange({target}){
+      const {id , value} = target
+      console.log(id, value)
+      setForm({...form, [id]: value});
+    }
+
+    return <form onSubmit={handleSubmit}>
+      <label htmlFor="nome">Nome</label>
+        <input id="nome" type="text" value={form.nome} onChange={handleChange}/>
+        {form.nome}
+        <label htmlFor="email">Email</label>
+        <input id="email" type="email" value={form.email} onChange={handleChange}/>
+        {form.email}
+        <button>Enviar</button>
+    </form>
+};
+
+export default App;
